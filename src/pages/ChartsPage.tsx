ffb2624ba2070adb580825570afa233340597cca
yagizdanaci <danaciyagiz@gmail.com>
2025-01-26 15:36:48 +0300

    Set up React Router with Diagram and Charts pages

---
+++ src/pages/ChartsPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChartsPage: React.FC = () => {
+  return <h2>Charts Page (Recharts will go here)</h2>;
+};
+
+export default ChartsPage;
---
+++ src/pages/DiagramPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DiagramPage: React.FC = () => {
+  return <h2>Diagram Page (React Flow will go here)</h2>;
+};
+
+export default DiagramPage;
